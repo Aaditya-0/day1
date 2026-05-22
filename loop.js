0e77dad6b9505154ab68cf7c742c8e0ebb8f283d
Aaditya-0 <181490486+Aaditya-0@users.noreply.github.com>
2026-05-22 12:22:42 +0530

    Loop js commit

---
+++ loop.js
@@ -0,0 +1,15 @@
+const table = 135698
+// console.log(table*1)
+// console.log(table*2)
+// console.log(table*3)
+// console.log(table*4)
+// console.log(table*5)
+// console.log(table*6)
+// console.log(table*7)
+// console.log(table*8)
+// console.log(table*9)
+// console.log(table*10)
+
+for(let i=1; i<=10; i++){
+    console.log(table*i)
+}
